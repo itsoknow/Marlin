3005a9d2345dcc6478c9213dd143a467d75734fe
itsoknow <129959205+itsoknow@users.noreply.github.com>
2023-05-10 21:49:53 +0800

    增加手动网床补偿
    设定补偿时，应当将热床的温度设置为实际工作时的温度。

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1353,7 +1353,7 @@
  * Use G29 repeatedly, adjusting the Z height at each point with movement commands
  * or (with LCD_BED_LEVELING) the LCD controller.
  */
-//#define PROBE_MANUALLY
+#define PROBE_MANUALLY // kk 20230421
 
 /**
  * A Fix-Mounted Probe either doesn't deploy or needs manual deployment.
@@ -1533,7 +1533,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 10, 10, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { 0, 0, 0 } // kk 20230421
 
 // Enable and set to use a specific tool for probing. Disable to allow any tool.
 #define PROBING_TOOL 0
@@ -1972,14 +1972,14 @@
 //#define AUTO_BED_LEVELING_LINEAR
 //#define AUTO_BED_LEVELING_BILINEAR
 //#define AUTO_BED_LEVELING_UBL
-//#define MESH_BED_LEVELING
+#define MESH_BED_LEVELING // kk 20230421
 
 /**
  * Normally G28 leaves leveling disabled on completion. Enable one of
  * these options to restore the prior leveling state or to always enable
  * leveling immediately after G28.
  */
-//#define RESTORE_LEVELING_AFTER_G28
+#define RESTORE_LEVELING_AFTER_G28 // kk 20230421
 //#define ENABLE_LEVELING_AFTER_G28
 
 /**
@@ -2020,7 +2020,7 @@
    */
   #define ENABLE_LEVELING_FADE_HEIGHT
   #if ENABLED(ENABLE_LEVELING_FADE_HEIGHT)
-    #define DEFAULT_LEVELING_FADE_HEIGHT 10.0 // (mm) Default fade height.
+    #define DEFAULT_LEVELING_FADE_HEIGHT 0.6 // (mm) Default fade height. // 三层0.6mm，好像不显示小数点 // kk 20230421
   #endif
 
   /**
@@ -2124,9 +2124,9 @@
   //=================================== Mesh ==================================
   //===========================================================================
 
-  #define MESH_INSET 10          // Set Mesh bounds as an inset region of the bed
-  #define GRID_MAX_POINTS_X 3    // Don't use more than 7 points per axis, implementation limited.
-  #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
+  #define MESH_INSET 5          // Set Mesh bounds as an inset region of the bed // 离热床边缘5mm // kk 20230421
+  #define GRID_MAX_POINTS_X 5    // Don't use more than 7 points per axis, implementation limited. // kk 20230421
+  #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X // kk 20230421
 
   //#define MESH_G28_REST_ORIGIN // After homing all axes ('G28' or 'G28 XYZ') rest Z at Z_MIN_POS
 
@@ -2136,12 +2136,12 @@
  * Add a bed leveling sub-menu for ABL or MBL.
  * Include a guided procedure if manual probing is enabled.
  */
-//#define LCD_BED_LEVELING
+#define LCD_BED_LEVELING // kk 20230421
 
 #if ENABLED(LCD_BED_LEVELING)
-  #define MESH_EDIT_Z_STEP  0.025 // (mm) Step size while manually probing Z axis.
+  #define MESH_EDIT_Z_STEP  0.005 // (mm) Step size while manually probing Z axis. // kk 20230421
   #define LCD_PROBE_Z_RANGE 4     // (mm) Z Range centered on Z_MIN_POS for LCD Z adjustment
-  //#define MESH_EDIT_MENU        // Add a menu to edit mesh points
+  #define MESH_EDIT_MENU        // Add a menu to edit mesh points // kk 20230421
 #endif
 
 // Add a menu item to move between bed corners for manual bed adjustment
